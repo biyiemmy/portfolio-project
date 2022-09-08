b8cd3d91425eeea032d71ed6646a5be4f9b5b5f8
adebiyiemmanuel11@gmail.com <adebiyiemmanuel11@gmail.com>
2022-09-08 10:48:35 +0100

    fourth commit

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -2,7 +2,7 @@ import NavBar from "../Components/NavBar";
 import AboutMe from "./AboutMe";
 import Skills from "../Components/Skills";
 import Services from "./Services";
-import Testimony from "../Components/Testimony";
+// import Testimony from "../Components/Testimony";
 import ContactMe from "./ContactMe";
 import Footer from "../Components/Footer";
 import Home from "../Components/Home";
